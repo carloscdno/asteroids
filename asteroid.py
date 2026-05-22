bf5a3350b2c2535abd3841e398eff000f7c4fd6f
carloscdno <elcordata@gmail.com>
2026-05-22 18:16:13 -0400

    Finished the whole thing

---
+++ asteroid.py
@@ -0,0 +1,38 @@
+import random 
+
+import pygame
+from logger import log_event
+from circleshape import CircleShape
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS, ASTEROID_MAX_RADIUS
+
+
+class Asteroid(CircleShape):
+    def __init__(self, x: float, y: float, radius: float) -> None:
+        super().__init__(x, y, radius)
+
+    def draw(self, screen: pygame.Surface) -> None:
+        pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
+
+    def update(self, dt: float) -> None:
+        self.position += self.velocity * dt
+    
+    def split(self):
+        self.kill()
+        
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+
+        log_event("asteroid_split")
+
+        random_angle = random.uniform(20, 50)
+
+        a = self.velocity.rotate(random_angle)
+        b = self.velocity.rotate(-random_angle)
+
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid.velocity = a * 1.2
+        asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid.velocity = b * 1.2
+        
+        
---
+++ circleshape.py
@@ -0,0 +1,32 @@
+import pygame
+
+# Base class for game objects
+class CircleShape(pygame.sprite.Sprite):
+    containers: tuple[pygame.sprite.Group, ...]
+
+    def __init__(self, x: float, y: float, radius: float) -> None:
+        # we will be using this later
+        if hasattr(self, "containers"):
+            super().__init__(*self.containers)
+        else:
+            super().__init__()
+
+        self.position: pygame.Vector2 = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+
+    def draw(self, screen: pygame.Surface) -> None:
+        # must override
+        pass
+
+    def update(self, dt: float) -> None:
+        # must override
+        pass
+
+    def collides_with(self, other):
+        distance_between_self_and_other = self.position.distance_to(other.position)
+        sum_radius = self.radius + other.radius
+        if distance_between_self_and_other <= sum_radius:
+            return True
+        return False
+    
